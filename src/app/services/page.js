2d0f62c3da3a84436abba1b8d37c928822e16d8d
Harsh Kumar <harshbkp123@gmail.com>
2024-12-19 16:08:21 +0530

    Basic Frames of Site

---
+++ src/app/services/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+import Link from "next/link";
+import Footer from "../components/footer.jsx";
+
+export default function Home() {
+    return (
+        <div>
+            Services
+            <Footer />
+        </div>
+    );
+}
