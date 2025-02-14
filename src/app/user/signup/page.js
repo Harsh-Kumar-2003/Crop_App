55017d4168eb0e77b299bbe6fbe6fb600e03e1dd
Shrehya <121685887+Shrehya@users.noreply.github.com>
2025-02-14 07:14:55 +0530

    Login and Sign Up page modification

--- src/app/user/signup/page.js
+++ src/app/user/signup/page.js
@@ -1,67 +1,59 @@
 //user/signup
 import SignUpForm from "@/components/forms/SignUpForm";
-import LoginWithGoogle from "@/components/LoginWithGoogle";
-import crop from "@/public/images/crop.jpg";
-import signUp from "@/public/images/signUp.jpg";
-import leaf1 from "@/public/images/leaf1.png";
+import login from "@/public/images/login.jpg";
 import Image from "next/image";
-import bg from "@/public/images/bg_signup.jpg";
-import logo from "@/public/images/fieldmaven.jpg";
 export default function SignUp() {
   return (
-    <div className=" h-screen">
-      <section
-        className=" bg-stone-500 "
-        style={{
-          backgroundImage: `url(${bg.src})`,
-          backgroundSize: "cover",
-        }}
-      >
-        <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
-          <aside className="relative block h-16  lg:col-span-5 lg:h-full xl:col-span-6">
-            <Image alt="image" src={logo} width={80} height={80}></Image>
+    <nav
+      className="bg-cover bg-center bg-no-repeat md:bg-cover"
+      style={{
+        backgroundImage: `url(${login.src})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}
+    >
+      <aside className="pl-3 pt-2 relative block h-16  lg:col-span-5 lg:h-full xl:col-span-6">
+        <Image
+          src="/fieldmaven1.png"
+          alt="A description of the image"
+          width={70}
+          height={70}
+          className="rounded-2xl "
+        ></Image>
+      </aside>
+      <div className="flex">
+        {/* // <div className="w-5/12 h-96 rounded-3xl bg-white bg-opacity-20 relative top-20 left-20">
+        //   <p
+        //     className="justify-center items-center text-3xl font-semibold
+        //     text-white mx-9 relative top-20
+        //     "
+        //     style={{
+        //       fontFamily: "'Dancing Script', cursive",
+        //       letterSpacing: "0.5px",
+        //       textShadow: "0.5px 0.5px 2px rgba(0, 0, 0, 0.1)",
+        //     }}
+        //   >
+        //     Welcome to FieldMaven – Your Trusted Companion for Smarter Farming.
+        //     Sign Up to unlock insights, optimize yields, and lead the way to
+        //     sustainable success.
+        //   </p>
+        // </div> */}
 
-            {/* <img
-              alt=""
-              src="/signup_image.jpeg"
-              className="absolute inset-0 h-full w-full object-cover"
-            /> */}
-          </aside>
-
-          <main
-            className="bg-stone-800 bg-opacity-15 rounded-2xl flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
-            style={{
-              fontFamily: "'Dancing Script', cursive",
-              color: "#4a4a4a",
-              letterSpacing: "0.5px",
-              textShadow: "0.5px 0.5px 2px rgba(0, 0, 0, 0.1)",
-            }}
-          >
-            <div className="max-w-xl lg:max-w-3xl">
-              {/* <img className="h-16 w-16" alt="logo" src="logo" /> */}
-              {/* <h1 className="mt-6 text-2xl font-bold text-yellow-300 sm:text-3xl md:text-4xl ">
-                Welcome to FieldMaven
-              </h1>
-
-              <p
-                className="mt-4 text-yellow-400 text-lg font-serif italic"
-                style={{
-                  fontFamily: "'Dancing Script', cursive",
-                  letterSpacing: "0.5px",
-                  textShadow: "0.5px 0.5px 2px rgba(0, 0, 0, 0.1)",
-                }}
-              >
-                "Welcome to FieldMaven – Your Trusted Companion for Smarter
-                Farming. Log in to unlock insights, optimize yields, and lead
-                the way to sustainable success."
-              </p> */}
-
-              <SignUpForm />
-              {/* <LoginWithGoogle /> */}
-            </div>
-          </main>
-        </div>
-      </section>
-    </div>
+        <main
+          className=""
+          style={{
+            fontFamily: "'Dancing Script', cursive",
+            color: "#4a4a4a",
+            letterSpacing: "0.5px",
+            textShadow: "0.5px 0.5px 2px rgba(0, 0, 0, 0.1)",
+          }}
+        >
+          <div className="max-w-xl lg:max-w-3xl">
+            <SignUpForm />
+          </div>
+        </main>
+      </div>
+    </nav>
   );
 }
--- src/components/forms/SignUpForm.js
+++ src/components/forms/SignUpForm.js
@@ -27,14 +27,6 @@ const SignUpForm = () => {
     e.preventDefault();
     const { firstName, lastName, email, password } = formData;
 
-    if (password.length < 8) {
-      alert("Password must be at least 8 characters long.");
-      return;
-    }
-    if (password !== confirmPassword) {
-      alert("Passwords do not match.");
-      return;
-    }
     const name = `${firstName} ${lastName}`;
     try {
       const result = await signIn("credentials", {
@@ -52,155 +44,156 @@ const SignUpForm = () => {
         alert("Login successful! Redirecting...");
       }
     } catch (error) {
-      alert("Something went wrong. Please try again.");
+      alert("Something went wrong. Please try again.");
+    }
+
+    if (password.length < 8) {
+      alert("Password must be at least 8 characters long.");
+      return;
+    }
+    if (password !== confirmPassword) {
+      alert("Passwords do not match.");
+      return;
     }
   };
 
   return (
-    <div>
-      <div>
-        <p className="text-center font-semibold text-4xl rounded-md border border-stone-500 bg-stone-500 text-white h-14 w-96 content-center relative left-32">
-          Create Account
-        </p>
-        <form onSubmit={handleSubmit} className="mt-8 grid grid-cols-6 gap-6">
-          <div className="col-span-6 ">
-            <label
-              htmlFor="FirstName"
-              className="block text-sm font-medium text-gray-700 dark:text-gray-200"
-            >
-              Full Name
-            </label>
-            <input
-              type="text"
-              id="FirstName"
-              name="firstName" // Update name to match formData
-              value={formData.firstName} // Bind value to formData
-              onChange={handleChange} // Use handleChange
-              className="mt-1 w-full rounded-md  bg-stone-200 text-sm text-gray-700 shadow-sm  dark:bg-gray-800 dark:text-gray-200 p-2"
-            />
-          </div>
+    <div className="ml-24">
+      <p className="pt-7 text-center font-semibold text-5xl text-white">
+        Create Account
+      </p>
+      <form onSubmit={handleSubmit} className="text-white item-center pt-5">
+        <div className="col-span-6 mx-16">
+          <p
+            htmlFor="FirstName"
+            className="text-sm font-medium text-white dark:text-white  mt-2"
+          >
+            First Name
+          </p>
+          <input
+            type="text"
+            id="FirstName"
+            name="firstName" // Update name to match formData
+            value={formData.firstName} // Bind value to formData
+            onChange={handleChange} // Use handleChange
+            className="pl-4 bg-white bg-opacity-20 text-white  w-full rounded-lg h-10"
+          />
+        </div>
 
-          {/* <div className="col-span-6 sm:col-span-3">
-          <label
+        <div className="col-span-6 mx-16">
+          <p
             htmlFor="LastName"
-            className="block text-sm font-medium text-gray-700 dark:text-gray-200"
+            className="text-sm font-medium text-white dark:text-white  mt-3"
           >
             Last Name
-          </label>
+          </p>
           <input
             type="text"
             id="LastName"
             name="lastName" // Update name to match formData
             value={formData.lastName} // Bind value to formData
             onChange={handleChange} // Use handleChange
-            className="mt-1 w-full rounded-md border-gray-300 bg-white text-sm text-gray-700 shadow-sm dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 p-2"
+            className="pl-4 bg-white bg-opacity-20 text-white w-full rounded-lg h-10"
           />
-        </div> */}
+        </div>
 
-          <div className="col-span-6">
-            <label
-              htmlFor="Email"
-              className="block text-sm font-medium text-gray-700 dark:text-gray-200"
-            >
-              Email
-            </label>
-            <input
-              type="email"
-              id="Email"
-              name="email" // Update name to match formData
-              value={formData.email} // Bind value to formData
-              onChange={handleChange} // Use handleChange
-              className="mt-1 w-full rounded-md  bg-stone-200 text-sm text-gray-700 shadow-sm  dark:bg-gray-800 dark:text-gray-200 p-2"
-            />
-          </div>
-
-          <div className="col-span-6 w-11/12">
-            <label
-              htmlFor="Password"
-              className="block text-sm font-medium text-gray-700 dark:text-gray-200"
-            >
-              Password
-            </label>
-            <input
-              type={showPassword ? "text" : "password"}
-              id="Password"
-              name="password" // Update name to match formData
-              value={formData.password} // Bind value to formData
-              onChange={handleChange} // Use handleChange
-              className="bg-stone-200 mt-1 w-full rounded-md text-sm text-gray-700 shadow-sm  dark:bg-gray-800 dark:text-gray-200 p-2"
-            />
-            <button
-              type="button"
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute inset-y-0 right-20 flex items-center pr-1 pt-9"
-            >
-              {showPassword ? (
-                <EyeOffIcon className="h-5 w-5 text-gray-500" />
-              ) : (
-                <EyeIcon className="h-5 w-5 text-gray-500" />
-              )}
-            </button>
-          </div>
-
-          <div className="col-span-6 ">
-            <label
-              htmlFor="PasswordConfirmation"
-              className="block text-sm font-medium text-gray-700 dark:text-gray-200"
-            >
-              Password Confirmation
-            </label>
-            <input
-              type="password"
-              id="PasswordConfirmation"
-              name="password_confirmation" // This can remain as is
-              value={confirmPassword} // Bind value to confirmPassword state
-              onChange={(e) => setConfirmPassword(e.target.value)} // Update confirmPassword state
-              className="mt-1 w-full rounded-md  bg-stone-200 text-sm text-gray-700 shadow-sm  dark:bg-gray-800 dark:text-gray-200 p-2"
-            />
-          </div>
-
-          <div className="col-span-6">
-            <p className="text-sm text-gray-500 dark:text-gray-400">
-              By creating an account, you agree to our
-              <a
-                href="#"
-                className="text-gray-700 underline dark:text-gray-200"
-              >
-                {" "}
-                terms and conditions{" "}
-              </a>
-              and
-              <a
-                href="#"
-                className="text-gray-700 underline dark:text-gray-200"
-              >
-                {" "}
-                privacy policy{" "}
-              </a>
-              .
-            </p>
-          </div>
-
-          <div className="justify-center col-span-6 sm:flex sm:items-center sm:gap-4">
-            <button
-              type="submit" // Ensure the button type is set to "submit"
-              className=" rounded-md border border-stone-500 bg-stone-500 px-12 py-3 text-sm font-medium text-white transition hover:bg-blue-200 hover:text-blue-600  "
-            >
-              Create an account
-            </button>
-          </div>
-          <p className="w-96 mx-28 text-center text-sm text-gray-500 sm:mt-0 dark:text-gray-400">
-            Already have an account ?
-            <a
-              href="/user/login"
-              className="text-gray-700 underline dark:text-gray-200"
-            >
-              {" "}
-              Log in{" "}
-            </a>
+        <div className="col-span-6 mx-16">
+          <p
+            htmlFor="Email"
+            className="text-sm font-medium text-white dark:text-white  mt-3"
+          >
+            Email
           </p>
-        </form>
-      </div>
+          <input
+            type="email"
+            id="Email"
+            name="email" // Update name to match formData
+            value={formData.email} // Bind value to formData
+            onChange={handleChange} // Use handleChange
+            className="pl-4 bg-white bg-opacity-20 text-white  w-full rounded-lg h-10"
+          />
+        </div>
+
+        <div className="col-span-6 mx-16 relative ">
+          <p
+            htmlFor="Password"
+            className="text-sm font-medium text-white dark:text-white  mt-3"
+          >
+            Password
+          </p>
+          <input
+            type={showPassword ? "text" : "password"}
+            id="Password"
+            name="password" // Update name to match formData
+            value={formData.password} // Bind value to formData
+            onChange={handleChange} // Use handleChange
+            className="pl-4 bg-white bg-opacity-20 text-white w-full rounded-lg h-10"
+          />
+          <button
+            type="button"
+            onClick={() => setShowPassword(!showPassword)}
+            className="absolute right-3 top-10 transform -translate-y-1/2"
+          >
+            {showPassword ? (
+              <EyeOffIcon className="h-7 text-white" />
+            ) : (
+              <EyeIcon className="h-7 text-white" />
+            )}
+          </button>
+        </div>
+
+        <div className="col-span-6 mx-16 mt-12">
+          <p
+            htmlFor="PasswordConfirmation"
+            className="text-sm font-medium text-white dark:text-white -mt-8 "
+          >
+            Confirme Password
+          </p>
+          <input
+            type="password"
+            id="PasswordConfirmation"
+            name="password_confirmation" // This can remain as is
+            value={confirmPassword} // Bind value to confirmPassword state
+            onChange={(e) => setConfirmPassword(e.target.value)} // Update confirmPassword state
+            className="pl-4 bg-white bg-opacity-20 text-white  w-full rounded-lg h-10"
+          />
+        </div>
+
+        <div className="col-span-6 mx-16">
+          <p className="text-sm text-white dark:text-white  mt-4">
+            By creating an account, you agree to our
+            <a href="#" className="text-red-700 underline dark:text-red-600">
+              {" "}
+              terms and conditions{" "}
+            </a>
+            and
+            <a href="#" className="text-red-700 underline dark:text-red-500">
+              {" "}
+              privacy policy{" "}
+            </a>
+            .
+          </p>
+        </div>
+
+        <div className="justify-center col-span-6 sm:flex sm:items-center sm:gap-4">
+          <button
+            type="submit" // Ensure the button type is set to "submit"
+            className=" rounded-md border border-green-500 bg-green-500 px-12 py-3 text-sm font-medium text-white  transition hover:bg-blue-200 hover:text-blue-600  "
+          >
+            Create an account
+          </button>
+        </div>
+        <p className="text-center text-sm text-white dark:text-white  mt-4 pb-14">
+          Already have an account ?
+          <a
+            href="/user/login"
+            className="text-red-700 underline dark:text-red-500"
+          >
+            {" "}
+            Log in{" "}
+          </a>
+        </p>
+      </form>
     </div>
   );
 };
