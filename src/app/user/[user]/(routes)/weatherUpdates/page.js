80d9c8c2a11eec6be1b0d83398b15f54cdb5bd27
Shrehya <121685887+Shrehya@users.noreply.github.com>
2025-03-29 15:21:42 +0530

    weather page design

--- src/app/user/[user]/(routes)/weatherUpdates/page.js
+++ src/app/user/[user]/(routes)/weatherUpdates/page.js
@@ -1,4 +1,19 @@
+import {
+  Sun,
+  CloudRain,
+  Wind,
+  Thermometer,
+  Sunrise,
+  Sunset,
+  Droplet,
+} from "lucide-react";
 import WeatherCard from "@/components/Card/weatherCardLarge";
+import Image from "next/image";
+import sunny from "@/public/images/sunny.png";
+import rainy from "@/public/images/rainy.png";
+import thunder from "@/public/images/thunder.png";
+import wind from "@/public/images/wind.png";
+import bg from "@/public/images/bg2.png";
 
 export default async function WeatherUpdates({ params }) {
   const { user } = await params;
@@ -25,22 +40,92 @@ export default async function WeatherUpdates({ params }) {
   const todayWeather = await weather.today;
   const forecast = await weather.forecast;
 
-  return (
-    <div className="p-6 space-y-4">
+  const getWeatherImage = () => {
+    if (todayWeather.maxTemperature >= 30) return sunny;
+    if (todayWeather.precipitationProbabilityMax >= 50) return rainy;
+    if (todayWeather.maxWindSpeed >= 7) return wind;
+    if (todayWeather.maxTemperature < 10) return thunder;
+    return sunny; // Default image
+  };
 
-      {/* Today's Weather */}
-      <WeatherCard todayWeather={todayWeather} date={"Today's Weather" } />
+  return (
+    <div
+      className="p-6 space-y-4 "
+      style={{
+        backgroundImage: `url(${bg.src})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="">
+        <div className="mt-4 flex gap-2 items-center justify-center">
+          <div className=" flex ">
+            <div className="mr-16 ml-6 ">
+              <Image src={getWeatherImage()} width={250} alt="sunny" />
+            </div>
+            <div className="">
+              <p className="text-7xl text-black font-bold mr-10 pb-4">
+                Today's Weather
+              </p>
+              <div className="flex items-center justify-between">
+                <p className="text-5xl font-bold text-blue-700 w-full">
+                  {todayWeather.maxTemperature}°C
+                </p>
+              </div>
+              <div className="flex items-center justify-between">
+                <p className="font-bold text-4xl w-full text-gray-500">
+                  min: {todayWeather.minTemperature}°C
+                </p>
+              </div>
+            </div>
+            <div className=" hidden md:flex md:flex-col pt-10 text-gray-800 font-semibold  w-52">
+              <div className="flex ">
+                <Sunrise size={20} className="mr-3 mb-4" />
+                <p>Sunrise : {todayWeather.sunrise}</p>
+              </div>
+              <div className="flex  ">
+                <Sunset size={20} className="mr-3 mb-4" />
+                <p>Sunset : {todayWeather.sunset}</p>
+              </div>
+              <div className="flex  ">
+                <CloudRain size={20} className="mr-3 mb-4" />
+                <p>CloudRain : {todayWeather.precipitationProbabilityMax}%</p>
+              </div>
+              <div className="flex  ">
+                <Wind size={20} className="mr-3 mb-4 " />
+                <p>Wind : {todayWeather.maxWindSpeed} km/h</p>
+              </div>
+              <div className="flex">
+                <Droplet size={20} className="mr-3 mb-4" />
+                <p>Droplet : {todayWeather.maxWindGusts} km/h</p>
+              </div>{" "}
+            </div>
+          </div>
+        </div>
+
+        {/* Today's Weather */}
+
+        {/* <WeatherCard
+          todayWeather={todayWeather}
+          date={"Today's Weather"}
+        /> */}
+      </div>
 
       {/* Forecast */}
-      <div className="space-y-4 bg-white">
-        <h1 className="text-4xl text-bold pt-10 text-green-800">Forecast</h1>
-        <div
-          className="flex flex-wrap gap-10 p-4 rounded-lg shadow-md"
-        >
-        {forecast.map((day, index) => (
-          <WeatherCard key={index } todayWeather={day} date={new Date(day.date).toDateString()} />
+      <div className="space-y-4 bg-white rounded-lg">
+        <h1 className="text-4xl font-bold pt-10 text-blue-700 pl-6">
+          Forecast
+        </h1>
+        <div className="flex flex-wrap gap-10 p-4 rounded-lg shadow-md">
+          {forecast.map((day, index) => (
+            <WeatherCard
+              key={index}
+              todayWeather={day}
+              date={new Date(day.date).toDateString()}
+            />
           ))}
-          </div>
+        </div>
       </div>
     </div>
   );
--- src/components/Card/weatherCardLarge.js
+++ src/components/Card/weatherCardLarge.js
@@ -1,37 +1,76 @@
-import { Sun, CloudRain, Wind, Thermometer, Sunrise, Sunset, Droplet } from "lucide-react";
+import {
+  Sun,
+  CloudRain,
+  Wind,
+  Thermometer,
+  Sunrise,
+  Sunset,
+  Droplet,
+} from "lucide-react";
+import sunny from "@/public/images/sunny.png";
+import Image from "next/image";
+import rainy from "@/public/images/rainy.png";
+import thunder from "@/public/images/thunder.png";
+import wind from "@/public/images/wind.png";
 
-export default function WeatherCard({ todayWeather ,date}) {
+export default function WeatherCard({ todayWeather, date }) {
+  const getWeatherImage = () => {
+    if (todayWeather.maxTemperature >= 30) return sunny;
+    if (todayWeather.precipitationProbabilityMax >= 50) return rainy;
+    if (todayWeather.maxWindSpeed >= 7) return wind;
+    if (todayWeather.maxTemperature < 10) return thunder;
+    return sunny; // Default image
+  };
+
   return (
     <div className="p-6 rounded-2xl shadow-xl bg-gradient-to-br from-blue-400 to-indigo-500 text-white max-w-sm w-full mx-auto">
-          <h2 className="text-2xl font-bold text-center">{date}</h2>
       <div className="mt-4 flex flex-col gap-2">
-        <div className="flex items-center justify-between">
-          <Thermometer size={20} />
-          <p>Max Temp: {todayWeather.maxTemperature}°C</p>
+        <div className=" flex ">
+          <div className="">
+            <h2 className="text-3xl font-semibold mb-2">{date}</h2>
+            <div className="flex items-center justify-between">
+              {/* <Thermometer size={20} /> */}
+              <p className="text-3xl font-bold text-yellow-300 w-full">
+                {todayWeather.maxTemperature}°C
+              </p>
+            </div>
+            <div className="flex items-center justify-between">
+              {/* <Thermometer size={20} /> */}
+              <p className="font-bold w-full">
+                {todayWeather.minTemperature}°C
+              </p>
+            </div>
+          </div>
+          <div className="">
+            <Image src={getWeatherImage()} width={150} alt="sunny" />
+          </div>
         </div>
-        <div className="flex items-center justify-between">
-          <Thermometer size={20} />
-          <p>Min Temp: {todayWeather.minTemperature}°C</p>
-        </div>
-        <div className="flex items-center justify-between">
-          <Sunrise size={20} />
-          <p>Sunrise: {todayWeather.sunrise}</p>
-        </div>
-        <div className="flex items-center justify-between">
-          <Sunset size={20} />
-          <p>Sunset: {todayWeather.sunset}</p>
-        </div>
-        <div className="flex items-center justify-between">
-          <CloudRain size={20} />
-          <p>Precipitation: {todayWeather.precipitationProbabilityMax}%</p>
-        </div>
-        <div className="flex items-center justify-between">
-          <Wind size={20} />
-          <p>Max Wind Speed: {todayWeather.maxWindSpeed} km/h</p>
-        </div>
-        <div className="flex items-center justify-between">
-          <Droplet size={20} />
-          <p>Max Wind Gusts: {todayWeather.maxWindGusts} km/h</p>
+        <hr />
+        <div className="">
+          <div className="flex items-center justify-between">
+            <div className="flex ">
+              <Sunrise size={20} className="mr-3" />
+              <p>{todayWeather.sunrise}</p>
+            </div>
+            <div className="flex ">
+              <Sunset size={20} className="mr-3" />
+              <p>{todayWeather.sunset}</p>
+            </div>
+          </div>
+          <div className="flex">
+            <div className="flex w-1/3">
+              <CloudRain size={20} className="mr-3" />
+              <p>{todayWeather.precipitationProbabilityMax}%</p>
+            </div>
+            <div className="flex w-1/3">
+              <Wind size={20} className="mr-3" />
+              <p>{todayWeather.maxWindSpeed} km/h</p>
+            </div>
+            <div className="flex w-1/3">
+              <Droplet size={20} className="mr-3" />
+              <p>{todayWeather.maxWindGusts} km/h</p>
+            </div>{" "}
+          </div>
         </div>
       </div>
     </div>
